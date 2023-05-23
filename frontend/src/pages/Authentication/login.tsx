a2583821dd735db8563d369e2fc418346ded3072
Oliver Flemmer <oli.flemmer@gmail.com>
2023-05-23 09:48:32 +0100

    Some basic frontent

--- frontend/src/pages/Authentication/login.tsx
+++ frontend/src/pages/Authentication/login.tsx
@@ -7,10 +7,15 @@ const Login = () => {
   };
 
   return (
-    <>
-      <img src={LargeLogo} alt="logo" className="logo" />
-      <Button onClick={handleSubmit} text="Login" />
-    </>
+    <div className="h-screen w-screen flex flex-col justify-center items-center">
+      <img src={LargeLogo} alt="logo" className="mb-8 w-1/2 md:w-1/6" />
+      <div className="flex flex-col items-center drop-shadow-lg rounded-md bg-white py-8 w-11/12 md:w-1/2">
+        <div className="flex flex-col items-center w-11/12 md:w-2/3">
+          <h1 className="text-2xl font-bold text-gray-800">Welcome Back!</h1>
+          <div className="h-0.5 bg-gray-200 w-full my-6"></div>
+        </div>
+      </div>
+    </div>
   );
 };
 
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      dropShadow: {
+        lg: '4px 4px 16px rgba(0, 0, 0, 0.15)',
+      },
+    },
+    plugins: [],
   },
-  plugins: [],
-}
-
+};
