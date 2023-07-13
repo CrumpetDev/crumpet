95b8d15d9e91b13d7f50b1554816b9baf958443c
tomtitherington <tom.titherington@gmail.com>
2023-07-13 16:44:56 +0100

    Fix unused var linting error

--- frontend/src/pages/welcome/welcome.tsx
+++ frontend/src/pages/welcome/welcome.tsx
@@ -1,18 +1,8 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import SidebarMenu from 'components/sidebarMenu';
 
 const Welcome = () => {
-  return (
-    <SidebarMenu />
-    // <div className="container bg-gray-200 rounded-xl p-8">
-    //   <p className="text-3xl text-primary">Welcome!</p>
-    // <SidebarMenu text="some text"/>
-    //   <Link to="/login" className="text-primary">
-    //     Login Page
-    //   </Link>
-    // </div>
-  );
+  return <SidebarMenu />;
 };
 
 export default Welcome;
