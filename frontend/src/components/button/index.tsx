2a90ba7d7471c86fe0680d92c871010221d6886b
tomtitherington <tom.titherington@gmail.com>
2023-09-08 14:28:15 +0100

    Fix button alignment and sizing

--- frontend/src/components/button/index.tsx
+++ frontend/src/components/button/index.tsx
@@ -2,14 +2,16 @@ interface CustomButtonProps {
   text: string;
   onClick: () => void;
   styles?: string;
+	icon?: React.ReactElement;
 }
 
-const CustomButton = ({ text, onClick, styles }: CustomButtonProps) => {
+const CustomButton = ({ text, onClick, styles, icon }: CustomButtonProps) => {
   return (
     <button
       onClick={onClick}
-      className={`w-full rounded-md bg-gray-600 py-2 px-8 text-white ${styles}`}>
+      className={`flex items-center justify-center rounded-md bg-gray-600 py-2 px-8 text-white ${styles}`}>
       {text}
+			{icon && <span className="ml-4">{icon}</span>}
     </button>
   );
 };
