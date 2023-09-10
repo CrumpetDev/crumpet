b2c42e8f50e59209abbecb0d40d768b8d10298bb
tomtitherington <tom.titherington@gmail.com>
2023-09-10 21:56:14 +0100

    Add api_key field to project model and create UUID base model

--- backend/app/models/project.py
+++ backend/app/models/project.py
@@ -1,5 +1,8 @@
-from django.db import models
+# from django.utils.crypto import get_random_string
+import uuid
+
 from app.models import User
+from django.db import models
 
 
 class Project(models.Model):
@@ -8,6 +11,14 @@ class Project(models.Model):
         User,
         through="ProjectMembership",
         related_name="projects",
+    )
+    # TODO: Replace this with proper key generation
+    api_key = models.CharField(
+        max_length=256,
+        unique=True,
+        default=uuid.uuid4,
+        null=False,
+        blank=False,
     )
 
     def __str__(self):
