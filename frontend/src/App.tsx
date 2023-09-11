bc59ee6395f84bc0a42353aee25d74a3a740680e
tomtitherington <tom.titherington@gmail.com>
2023-09-11 20:51:05 +0100

    Refactor useRouter and add auth token context

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 // import { setOpenApiBase } from 'api/configOpenApi';
 import Router from './routes';
+import { TokenContext } from 'api';
 
 function App() {
+  const [accessToken, setAccessToken] = useState<string>('');
+  const [refreshToken, setRefreshToken] = useState<string>('');
+
   // Will not work until openapi is setup
   // useEffect(() => {
   //   setOpenApiBase();
   // }, []);
 
-  return <Router />;
+  return (
+    <TokenContext.Provider value={{ accessToken, setAccessToken, refreshToken, setRefreshToken }}>
+      <Router />
+    </TokenContext.Provider>
+  );
 }
 
 export default App;
--- frontend/src/routes/useRouter.tsx
+++ frontend/src/routes/useRouter.tsx
@@ -5,35 +5,65 @@ import { useCookies } from 'react-cookie';
 import NotFound from './404';
 import Login from 'pages/Authentication';
 import SidebarMenu from 'components/sidebarMenu';
+import Root from 'pages/root';
+import Settings from 'pages/settings/settings';
+import Flows from 'pages/flows';
+import { useAuthentication } from 'api';
+import ProtectedRoute from './ProtectedRoute';
 
 const useRouter = () => {
   const [cookies] = useCookies(['refreshToken']);
   const authenticated = cookies.refreshToken;
+  //const { authenticated } = useAuthentication();
   let routerList: RouteObject[] = [];
 
-  if (authenticated) {
-    routerList = [
-      {
-        path: '/',
-        element: <SidebarMenu />,
-      },
-      {
-        path: '*',
-        element: <NotFound />,
-      },
-    ];
-  } else {
-    routerList = [
-      {
-        path: '/',
-        element: <Login />,
-      },
-      {
-        path: '*',
-        element: <Navigate to="/login" />,
-      },
-    ];
-  }
+  routerList = [
+    {
+      path: '/',
+      element: <ProtectedRoute> <Root /> </ProtectedRoute>,
+      children: [
+        { path: '/', element: <Navigate to="flows" replace /> },
+				{ path: 'flows', element: <Flows />},
+        { path: 'settings', element: <Settings /> },
+      ],
+    },
+		{
+			path: '/login',
+			element: <Login />
+		},
+    {
+      path: '*',
+      element: <NotFound />,
+    },
+  ];
+
+  // if (authenticated) {
+  //   routerList = [
+  //     {
+  //       path: '/',
+  //       element: <Root />,
+  //       children: [
+  //         { path: '/', element: <Navigate to="/settings" replace /> },
+  //         { path: 'settings', element: <Settings /> },
+  //       ],
+  //     },
+  //     {
+  //       path: '*',
+  //       element: <NotFound />,
+  //     },
+  //   ];
+  // } else {
+  //   routerList = [
+  //     {
+  //       path: '/',
+  //       element: <Login />,
+  //     },
+  //     {
+  //       path: '*',
+  //       element: <Navigate to="/login" />,
+  //     },
+  //   ];
+  // }
 
   const router = createHashRouter(routerList);
 
