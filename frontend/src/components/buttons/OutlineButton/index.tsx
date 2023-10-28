6f32e613df44df2e2343921fde56d686fe5e5e62
tomtitherington <tom.titherington@gmail.com>
2023-10-28 13:36:42 +0100

    Add onClick prop to OutlineButton

--- frontend/src/components/buttons/OutlineButton/index.tsx
+++ frontend/src/components/buttons/OutlineButton/index.tsx
@@ -1,11 +1,13 @@
 interface OutlineButtonProps {
   label: string;
   className?: string;
+  onClick?: () => void;
 }
 
-const OutlineButton = ({ label, className }: OutlineButtonProps) => {
+const OutlineButton = ({ label, className, onClick }: OutlineButtonProps) => {
   return (
     <button
+      onClick={onClick}
       className={`rounded outline outline-1 
               outline-red-100 p-2 text-red-700 text-sm font-medium ${className}`}>
       {label}
