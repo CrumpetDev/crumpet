7df2654a24b85b5c47d03c070a31ee33f0560c34
tomtitherington <tom.titherington@gmail.com>
2023-11-03 16:51:49 +0000

    Format serializer

--- backend/app/serializers/environment_serializer.py
+++ backend/app/serializers/environment_serializer.py
@@ -6,4 +6,9 @@ class EnvironmentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Environment
         fields = ["id", "name", "identifier", "is_default"]
-        extra_kwargs = {'id': {'required': True}, 'name': {'required': True}, 'identifier': {'required': True}, 'is_default': {'required': True} }
+        extra_kwargs = {
+            "id": {"required": True},
+            "name": {"required": True},
+            "identifier": {"required": True},
+            "is_default": {"required": True},
+        }
