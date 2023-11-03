b306c33b4a3eaf1ae888487554b92374fc0e322e
tomtitherington <tom.titherington@gmail.com>
2023-11-03 14:46:16 +0000

    Add explicit null and blank == false flags to model fields

--- backend/app/models/environment.py
+++ backend/app/models/environment.py
@@ -4,12 +4,12 @@ import uuid
 
 
 class Environment(models.Model):
-    name = models.CharField(max_length=100)
-    identifier = models.CharField(max_length=255, default=uuid.uuid4)
+    name = models.CharField(max_length=100, blank=False, null=False)
+    identifier = models.CharField(max_length=255, default=uuid.uuid4, blank=False, null=False)
     project = models.ForeignKey(
         "Project", on_delete=models.CASCADE, related_name="environments"
     )
-    is_default = models.BooleanField(default=False)
+    is_default = models.BooleanField(default=False, blank=False, null=False)
 
     class Meta:
         unique_together = ["identifier", "project"]
