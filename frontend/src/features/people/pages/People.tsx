455d53a0f672c3316ce184767975f87fbc72f846
tomtitherington <tom.titherington@gmail.com>
2023-11-08 21:16:37 +0000

    Remove dead code

--- frontend/src/features/people/pages/People.tsx
+++ frontend/src/features/people/pages/People.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import { Table } from '../components';
 
 const People = () => {
@@ -16,23 +15,9 @@ const People = () => {
     // ... more columns
   ];
 
-  // Using state to dynamically manage columns
-  const [columns, setColumns] = useState(columnJson);
-
-  // Handler to add a new column
-  const handleAddColumn = () => {
-    // Example of a new column. You might want to make this more dynamic.
-    const newColumn = {
-      accessor: `newColumn${columns.length}`, // Ensure the accessor is unique
-      header: `New Column ${columns.length}`,
-    };
-    setColumns(prevColumns => [...prevColumns, newColumn]);
-  };
-
   return (
     <div className="w-full h-full container px-6 py-8">
-      <span>People</span>
-      <Table data={data} columnJson={columnJson} />
+      <Table className="w-full h-full" data={data} columnJson={columnJson} />
     </div>
   );
 };
