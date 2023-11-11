e10cfb36680cdb472454f9216e355ed74eb3231b
tomtitherington <tom.titherington@gmail.com>
2023-11-11 10:28:48 +0000

    Add popover menu to the PropertyHeader component

--- frontend/src/features/people/components/table/headers/PropertyHeader.tsx
+++ frontend/src/features/people/components/table/headers/PropertyHeader.tsx
@@ -1,4 +1,8 @@
+import { Popover, Transition } from '@headlessui/react';
 import { Header, flexRender } from '@tanstack/react-table';
+import { SlimTextInput } from 'components/inputs';
+import { Fragment, useEffect, useState } from 'react';
+import { usePopper } from 'react-popper';
 
 interface PropertyHeaderProps<TData, TValue> {
   header: Header<TData, TValue>;
@@ -6,31 +10,106 @@ interface PropertyHeaderProps<TData, TValue> {
 }
 
 const PropertyHeader = <TData, TValue>({ header, value }: PropertyHeaderProps<TData, TValue>) => {
+  const [referenceElement, setReferenceElement] = useState<HTMLButtonElement | null>(null);
+  const [popperElement, setPopperElement] = useState<HTMLDivElement | null>(null);
+  const { styles, attributes } = usePopper(referenceElement, popperElement, {
+    placement: 'bottom',
+  });
+
+  //TODO: Get display name from store
+  const [displayName, setDisplayName] = useState('');
+
+  const [isDragging, setIsDragging] = useState(false);
+
+  useEffect(() => {
+    const handleMouseUp = () => {
+      if (isDragging) {
+        //Delay this so that it comes after the mouseup event on the Popover.Button
+        // i.e. so that the Popover.Button doesn't get clicked when dragging
+        setTimeout(() => setIsDragging(false), 0);
+      }
+    };
+
+    document.addEventListener('mouseup', handleMouseUp);
+
+    return () => {
+      document.removeEventListener('mouseup', handleMouseUp);
+    };
+  }, [isDragging]);
+
   return (
-    <div
-      className="th flex justify-between items-center p-2 border-r border-crumpet-light-200 
-                        text-grey-700 text-left text-sm font-medium whitespace-nowrap overflow-hidden"
-      key={header.id}
-      style={{
-        width: header.getSize(),
-        position: 'relative',
-      }}>
-      {value}
-      <div
-        {...{
-          onMouseDown: header.getResizeHandler(),
-          onTouchStart: header.getResizeHandler(),
-        }}
-        className={`resizer h-full w-1 cursor-col-resize select-none hover:bg-crumpet-yellow-500
+    <Popover>
+      {({ open }) => (
+        <>
+          <Popover.Button
+            onClick={event => {
+              if (isDragging) {
+                event.stopPropagation();
+                event.preventDefault();
+              }
+            }}
+            key={header.id}
+            ref={setReferenceElement}
+            className="th flex justify-between items-center p-2 border-r border-crumpet-light-200
+               text-grey-700 text-left text-sm font-medium whitespace-nowrap overflow-hidden 
+               hover:bg-crumpet-light-100 hover:cursor-pointer outline-none"
+            style={{
+              width: header.getSize(),
+              position: 'relative',
+            }}>
+            {value}
+            <div
+              {...{
+                onMouseDown: event => {
+                  setIsDragging(true);
+                  event.stopPropagation();
+                  header.getResizeHandler()(event);
+                },
+                onTouchStart: event => {
+                  event.stopPropagation();
+                  header.getResizeHandler()(event);
+                },
+              }}
+              className={`resizer h-full w-1 cursor-col-resize select-none hover:bg-crumpet-yellow-500
                             ${header.column.getIsResizing() ? 'bg-crumpet-yellow-500' : ''} `}
-        style={{
-          position: 'absolute',
-          right: 0,
-          top: 0,
-        }}
-      />
-    </div>
+              style={{
+                position: 'absolute',
+                right: 0,
+                top: 0,
+              }}
+            />
+          </Popover.Button>
+          <Popover.Panel>
+            <Transition
+              show={open}
+              as={Fragment}
+              enter="transition ease-out duration-200"
+              enterFrom="opacity-0 translate-y-1"
+              enterTo="opacity-100 translate-y-0"
+              leave="transition ease-in duration-150"
+              leaveFrom="opacity-100 translate-y-0"
+              leaveTo="opacity-0 translate-y-1">
+              <Popover.Panel
+                static
+                ref={setPopperElement}
+                style={styles.popper}
+                {...attributes.popper}
+                className="flex flex-col w-64 overflow-hidden p-4 gap-2 bg-white rounded-md shadow-lg ring-1 ring-black 
+                          ring-opacity-5">
+                <SlimTextInput
+                  label="Display Name"
+                  placeholder="Property name"
+                  onChange={e => setDisplayName(e.target.value)}
+                  inputProps={{ name: 'header' }}
+                />
+              </Popover.Panel>
+            </Transition>
+          </Popover.Panel>
+        </>
+      )}
+    </Popover>
   );
+
 };
 
 export default PropertyHeader;
