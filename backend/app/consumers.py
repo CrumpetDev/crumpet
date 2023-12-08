e571acced0e19f7534ba3990abfa63e8a89fc08f
tomtitherington <tom.titherington@gmail.com>
2023-12-08 11:32:36 +0000

    Create PersonSchema serializer and update consumer to use it

--- backend/app/consumers.py
+++ backend/app/consumers.py
@@ -1,6 +1,11 @@
 from channels.generic.websocket import AsyncWebsocketConsumer
 import json
 
+from .models import Environment, Person
+
+from .serializers import PersonSerializer, PersonSchemaSerializer
+
+
 class PeopleConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         await self.accept()
@@ -8,9 +13,29 @@ class PeopleConsumer(AsyncWebsocketConsumer):
     async def disconnect(self, close_code):
         pass
 
-    async def receive(self, text_data):
-        text_data_json = json.loads(text_data)
-        message = text_data_json['message']
-        print(message)
+    async def receive(self, data):
+        json = json.loads(data)
+        schema = json["schema"]
+        data = json["data"]
 
-        # Handle received message...
+        if data:
+            # Deserialize data using PersonSerializer
+            serializer = PersonSerializer(data=data)
+            if serializer.is_valid():
+                # Save or update the Person instance
+                serializer.save()
+                print("Person saved")
+            else:
+                # Handle invalid data case
+                print(serializer.errors)
+        
+        if schema:
+            # Deserialize schema using PersonSchemaSerializer
+            serializer = PersonSchemaSerializer(data=schema)
+            if serializer.is_valid():
+                # Save or update the PersonSchema instance
+                serializer.save()
+                print("Schema saved")
+            else:
+                # Handle invalid schema case
+                print(serializer.errors)
--- backend/app/serializers/__init__.py
+++ backend/app/serializers/__init__.py
@@ -1,2 +1,3 @@
 from .register_serializer import *
 from .person_serializer import PersonSerializer
+from .person_schema_serializer import PersonSchemaSerializer
