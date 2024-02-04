b7f91c444dfa80b16c5e8b9bbfff7506f9a261fe
tomtitherington <tom.titherington@gmail.com>
2024-02-04 15:11:28 +0000

    Remove unused models

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,18 +1,5 @@
-from .proxy import ProxyManager
-from .element import Element
-from .event import Event
-from .proxy_elements import (
-    Row,
-    Column,
-    TextElement,
-    ImageElement,
-    Slider,
-    TextField,
-    ButtonElement,
-)
 from .user import User
 from .project import Project, ProjectMembership
-from .selector import Selector
 from .environment import Environment
 from .person import Person
 from .person_schema import PersonSchema
