266988a3ef92bd1e8e0070c4f707789d140f5bb1
tomtitherington <tom.titherington@gmail.com>
2024-02-05 22:29:10 +0000

    Add docstring style comments to schema models

--- backend/app/models/flow_schema.py
+++ backend/app/models/flow_schema.py
@@ -1,16 +1,26 @@
+from typing import Optional
 from models import UUIDModel
 from django.db import models
 
 
 class FlowSchema(UUIDModel):
+    """
+    Represents a flow's schema, defining the structure of an engagement workflow.
+
+    Just like a database schema, a flow schema is a blueprint for creating flow instances. It defines the
+    steps and transitions that make up the flow. Each flow schema can have multiple versions and is tracked
+    by FlowSchemaVersion.
+    """
+
     identifier = models.CharField(max_length=50, unique=True, blank=False)
     current_version = models.ForeignKey("FlowSchemaVersion", on_delete=models.SET_NULL, null=True, related_name="+")
 
-    def latest_version(self):
-        # Retrieve the most recent version based on the 'created_at' timestamp
+    def latest_version(self) -> Optional["FlowSchemaVersion"]:
+        """Retrieves the most recent version of the flow schema based on the 'created_at' timestamp."""
         return self.versions.order_by("-created_at").first()
 
-    def revert_to_version(self, version_identifier):
+    def revert_to_version(self, version_identifier: str):
+        """Sets the current_version of the flow schema to the specified version_identifier, if it exists."""
         try:
             version = self.versions.get(version_identifier=version_identifier)
             self.current_version = version
@@ -20,6 +30,13 @@ class FlowSchema(UUIDModel):
 
 
 class StepSchema(UUIDModel):
+    """
+    Defines a step within a specific version of a flow schema.
+
+    Each step is a part of a flow and represents a possible state of the flow at a certain point in time. Steps
+    can also define an (optional) action to be taken.
+    """
+
     flow_schema_version = models.ForeignKey("FlowSchemaVersion", on_delete=models.CASCADE, related_name="steps")
     identifier = models.CharField(max_length=100, unique=True, blank=False)
     name = models.CharField(max_length=100, blank=False)
@@ -27,14 +44,29 @@ class StepSchema(UUIDModel):
 
 
 class TransitionSchema(UUIDModel):
+    """
+    Defines a unidirectional transition between steps in (a specific version of) a flow schema.
+
+    A transition connects (from_step) and (to_step) within a flow and are unidirectional. A transition signals that it 
+    is valid to move from one step (from_step) to another (to_step) if the optional condition is met.
+    """
+
     flow_schema_version = models.ForeignKey("FlowSchemaVersion", on_delete=models.CASCADE, related_name="transitions")
     identifier = models.CharField(max_length=100, unique=True, blank=False)
+    # TODO: Define TransitionType choices
+    type = models.CharField(max_length=50, blank=False)
     from_step = models.ForeignKey(StepSchema, on_delete=models.CASCADE, related_name="outgoing_transitions")
     to_step = models.ForeignKey(StepSchema, on_delete=models.CASCADE, related_name="incoming_transitions")
     condition = models.CharField(max_length=500, blank=True)
 
 
 class FlowSchemaVersion(UUIDModel):
+    """
+    Represents a specific version of a FlowSchema, allowing for versioning of schemas.
+
+    Each version can have multiple steps (StepSchema) and transitions (TransitionSchema).
+    """
+
     schema = models.ForeignKey(FlowSchema, on_delete=models.CASCADE, related_name="versions")
     description = models.CharField(max_length=250, blank=False)
     created_at = models.DateTimeField(auto_now_add=True)
