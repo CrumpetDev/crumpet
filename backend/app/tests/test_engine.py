8798dba877f93033afa9f9a09a36ddbb55c3d1b0
tomtitherington <tom.titherington@gmail.com>
2024-03-01 15:54:24 +0000

    Add test TODOs

---
+++ backend/app/tests/test_engine.py
@@ -0,0 +1,2 @@
+# TODO: Test to check that two identical flows for different People do not interfere with each other
+# TODO: Test that a person cannot have two active flows of the same schema at the same time
